7cee311c106630957b267607482434e0a5eb842f
balo-max <shoni6124@gmail.com>
2024-12-12 20:13:05 +0100

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const formElement = document.querySelector('.login-form');
 
+formElement.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (formElement.elements.email.value.trim() === '' || formElement.elements.password.value.trim() === '') {
+        alert('All form fields must be filled in');
+    }
+
+    const loginObj = {
+        [formElement.elements.email.name]: formElement.elements.email.value.trim(),
+        [formElement.elements.password.name]: formElement.elements.password.value.trim(),
+    }
+    console.log(loginObj);
+
+    formElement.reset();
+})
